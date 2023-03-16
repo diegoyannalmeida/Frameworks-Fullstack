b1a5f18fba91a9315cf1f71c3b5810755674e010
Diego Almeida <diego.yalmeida@hsl.org.br>
2023-03-15 21:13:15 -0300

    Atualização da logica

--- AC-01/Atividade-continua-01.py
+++ AC-01/Atividade-continua-01.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/media/<nota1>/<nota2>')
-def calcular_media(nota1, nota2):
-    media = (float(nota1) + float(nota2)) / 2
-    return f'A média das notas é {media}!'
+@app.route('/media', methods=['GET'])
+def calcular_media():
+    nota1 = float(request.args.get('nota1'))
+    nota2 = float(request.args.get('nota2'))
+
+    media = (nota1 + nota2) / 2
+    
+    return f'A média das notas {nota1:.2f} e {nota2:.2f} é: {media:.2f}'
 
 if __name__ == '__main__':
     app.run(debug=True)
